b19a150a31c8e107c91102c97375b06b8c63eacd
Corrine Chan <2317107204@qq.com>
2022-03-03 17:02:06 +0800

    first commit

---
+++ demo/demoDlg.cpp
@@ -0,0 +1,287 @@
+﻿
+// demoDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "demo.h"
+#include "demoDlg.h"
+#include "afxdialogex.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CdemoDlg 对话框
+
+
+
+CdemoDlg::CdemoDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DEMO_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CdemoDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CdemoDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CdemoDlg::OnBnClickedOk)
+	//ON_BN_CLICKED(IDC_BUTTON1, &CdemoDlg::OnBnClickedButton1)
+	//ON_BN_CLICKED(IDC_BUTTON2, &CdemoDlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_OpenCam, &CdemoDlg::OnBnClickedOpencam)
+	ON_BN_CLICKED(IDC_StartGrab, &CdemoDlg::OnBnClickedStartgrab)
+	ON_BN_CLICKED(IDC_CloseCam, &CdemoDlg::OnBnClickedClosecam)
+	ON_STN_CLICKED(pic, &CdemoDlg::OnStnClickedpic)
+END_MESSAGE_MAP()
+
+
+// CdemoDlg 消息处理程序
+
+BOOL CdemoDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	MVSTATUS_CODES r;
+	r = MVInitLib();
+	if (r != MVST_SUCCESS)
+	{
+		MessageBox(_T("函数库初始化失败！"), _T("提示"), MB_ICONWARNING);
+		return TRUE;
+	}
+	r = MVUpdateCameraList();
+	if (r != MVST_SUCCESS)
+	{
+		MessageBox(_T("查找连接计算机的相机失败！"), _T("提示"), MB_ICONWARNING);
+		return TRUE;
+	}
+	GetDlgItem(IDC_OpenCam)->EnableWindow(true);
+	GetDlgItem(IDC_StartGrab)->EnableWindow(false);
+	GetDlgItem(IDC_CloseCam)->EnableWindow(false);
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CdemoDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CdemoDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CdemoDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CdemoDlg::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
+
+void CdemoDlg::OnBnClickedOpencam()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	int nCams = 0;
+	MVGetNumOfCameras(&nCams);
+	if (nCams == 0)
+	{
+		MessageBox(_T(" 没 有 找 到 相 机 , 请确认连接和相机 IP 设 置 "), _T(" 提 示"),MB_ICONWARNING);
+			return;
+	}
+	MVSTATUS_CODES r = MVOpenCamByIndex(0, &m_hCam);
+	if (m_hCam == NULL)
+	{
+		if (r == MVST_ACCESS_DENIED)
+			MessageBox(_T(" 无 法 打 开 相 机 ， 可 能 正 被 别 的 软 件 控 制 "), _T(" 提 示 "),MB_ICONWARNING);
+		else
+			MessageBox(_T("无法打开相机"), _T("提示"), MB_ICONWARNING);
+		return;
+	}
+	int w, h;
+	MVGetWidth(m_hCam, &w);
+	MVGetHeight(m_hCam, &h);
+	MVGetPixelFormat(m_hCam, &m_PixelFormat);
+	m_image.CreateByPixelFormat(w, h, m_PixelFormat);
+	GetDlgItem(IDC_OpenCam)->EnableWindow(false);
+	GetDlgItem(IDC_StartGrab)->EnableWindow(true);
+	GetDlgItem(IDC_CloseCam)->EnableWindow(false);
+
+}
+void CdemoDlg::DrawImage()
+{
+	CRect rct;
+	GetDlgItem(pic)->GetClientRect(&rct);
+	int dstW = rct.Width();
+	int dstH = rct.Height();
+	CDC* pDC = GetDlgItem(pic)->GetDC();
+	{
+		pDC->SetStretchBltMode(COLORONCOLOR);
+		m_image.Draw(pDC->GetSafeHdc(), 0, 0, dstW, dstH);
+	}
+	ReleaseDC(pDC);
+}
+
+int CdemoDlg::OnStreamCB(MV_IMAGE_INFO* pInfo)
+{
+
+	MVInfo2Image(m_hCam, pInfo, &m_image);
+	DrawImage();
+	return 0;
+}
+
+int __stdcall StreamCB(MV_IMAGE_INFO* pInfo, ULONG_PTR nUserVal)
+{
+	CdemoDlg* pDlg = (CdemoDlg*)nUserVal;
+	return (pDlg->OnStreamCB(pInfo));
+}
+
+void CdemoDlg::OnBnClickedStartgrab()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	TriggerModeEnums enumMode;
+	MVGetTriggerMode(m_hCam, &enumMode);
+	if (enumMode != TriggerMode_Off)
+	{
+		MVSetTriggerMode(m_hCam, TriggerMode_Off);
+		Sleep(100);
+	}
+	MVStartGrab(m_hCam, (MVStreamCB)StreamCB, (ULONG_PTR)this);
+	m_bRun = true;
+	GetDlgItem(IDC_OpenCam)->EnableWindow(false);
+	GetDlgItem(IDC_StartGrab)->EnableWindow(false);
+	GetDlgItem(IDC_CloseCam)->EnableWindow(true);
+
+}
+
+void CdemoDlg::OnBnClickedClosecam()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	MVStopGrab(m_hCam);
+	MVCloseCam(m_hCam);
+	m_bRun = false;
+	GetDlgItem(IDC_OpenCam)->EnableWindow(true);
+	GetDlgItem(IDC_StartGrab)->EnableWindow(false);
+	GetDlgItem(IDC_CloseCam)->EnableWindow(false);
+}
+
+
+void CdemoDlg::OnStnClickedpic()
+{
+	// TODO: 在此添加控件通知处理程序代码
+}
+
+void CdemoDlg::OnClose()
+{
+	if (m_bRun != false)
+	{
+		MVStopGrab(m_hCam);
+	}
+	MVTerminateLib();
+	CDialog::OnClose();
+}
---
+++ demo/demoDlg.h
@@ -0,0 +1,60 @@
+﻿
+// demoDlg.h: 头文件
+//
+
+#pragma once
+#include "MVGigE.h"
+#include "MVImage.h"
+
+
+// CdemoDlg 对话框
+class CdemoDlg : public CDialogEx
+{
+// 构造
+public:
+	CdemoDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DEMO_DIALOG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP();
+
+public:
+	afx_msg void OnBnClickedOk();
+	//定义相机句柄对象
+	HANDLE m_hCam;
+	//定义图像处理对象
+	MVImage m_image;
+	//定义像素格式对象
+	MV_PixelFormatEnums m_PixelFormat;
+	//采集标识
+	bool m_bRun;
+	//通过回调函数获取图像数据信息
+	int OnStreamCB(MV_IMAGE_INFO* pInfo);
+	//画图
+	void DrawImage();
+		//关闭相机、释放资源
+	void OnClose();
+
+	afx_msg void OnBnClickedOpencam();
+	afx_msg void OnBnClickedStartgrab();
+	afx_msg void OnBnClickedClosecam();
+	afx_msg void OnStnClickedpic();
+
+
+};
